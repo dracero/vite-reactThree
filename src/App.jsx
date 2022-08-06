34f4342597756631002cfe26c1fcd222065ef3a9
dracero <diego.racero@gmail.com>
2022-08-06 17:12:21 -0300

    6

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import * as THREE from "three";
 import { Canvas } from '@react-three/fiber';
 
 export default function App(props) {
-  const { nodes, materials } = useGLTF("public/first.glb");
+  const { nodes, materials } = useGLTF("/first.glb");
   return (
     <Canvas camera={{ position: [0, 5, 0], fov: 70 }} shadows>
       <gridHelper />
@@ -18,20 +18,30 @@ export default function App(props) {
         <sphereBufferGeometry args={[1, 50, 50]} />
         <meshStandardMaterial color="#FF0000" side={THREE.DoubleSide} />
       </mesh>
-      <group {...props} dispose={null}>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Cylinder.geometry}
-        material={materials["Material.001"]}
-        position={[1.26, 0.4, -2.07]}
-        rotation={[2.63, 1, -1.13]}
-        scale={[1.02, 1.77, 1.04]}
-      />
+      <group dispose={null}>
+      <group name="Scene">
+        <mesh
+          name="Cube"
+          castShadow
+          receiveShadow
+          geometry={nodes.Cube.geometry}
+          material={materials.Material}
+          rotation={[0.08, 0.18, -0.13]}
+          scale={[1, 1.37, 3.09]}
+        />
+        <mesh
+          name="Cylinder"
+          castShadow
+          receiveShadow
+          geometry={nodes.Cylinder.geometry}
+          material={materials["Material.001"]}
+          position={[-0.19, 3.76, -2.19]}
+          rotation={[-3.14, -0.2, -1.46]}
+          scale={[1.02, 1.77, 1.04]}
+        />
+      </group>
     </group>
       <OrbitControls />
     </Canvas>
   );
 }
-
-useGLTF.preload("public/first.glb");
